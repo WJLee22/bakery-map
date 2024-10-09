9a214fcb1ecf87d8d5ad7d7b9d5e578ae31ab12a
WJ Lee <146986774+WJLee222@users.noreply.github.com>
2024-10-09 10:15:24 +0900

    edit head text to "가까운 빵집을 만나보세요!"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     return (
        /*컨테이너 안에 카카오맵 배치*/
         <div className='container'>
-            <h1>🥖🍞🥐 내 주변 빵집찾기~!! 🥐🍞🥖</h1>
+            <h1>🥖🍞🥐 가까운 빵집을 만나보세요! 🥐🍞🥖</h1>
             <KakaoMap /> {/* KakaoMap 컴포넌트 사용 */}
         </div>
     );
